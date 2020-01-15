b3fc3adc9b7ab9cc85348e2a0f1b75869f1591b0
Satnam Singh <satnam9781@gmail.com>
2020-01-15 22:39:55 +0530

    Fix route method

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,5 @@
 module.exports.routes = {
   'POST /login': 'AuthController.login',
   'POST /register': 'UserController.register',
-  'POST /user/fetch': 'UserController.fetch',
+  'GET /user/fetch': 'UserController.fetch',
 };
