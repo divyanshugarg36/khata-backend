fd87940ed19a24aef25b873c0b8c2db5c19c93f1
Divyanshu Garg <divyanshugarg36@gmail.com>
2020-01-27 23:58:27 +0530

    replaced urls

--- config/env/production.js
+++ config/env/production.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURL:'mongodb://superuser:superpass@khata-p3bjd.mongodb.net/khata?authSource=admin',
+  mongoURL:'mongodb+srv://superuser:superpass@khata-p3bjd.mongodb.net/khata?authSource=admin',
   sockets: {
     onlyAllowOrigins: ['http://localhost:3000','https://khata-ryaz.herokuapp.com']
   },
