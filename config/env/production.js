ab71293ce9ce21f0e542ade2ed29781223feaddc
Divyanshu Garg <divyanshugarg36@gmail.com>
2020-01-27 23:04:28 +0530

    Added url

--- config/env/production.js
+++ config/env/production.js
@@ -1,13 +1,13 @@
 module.exports = {
   'mongo':{
     host: 'khata-p3bjd.mongodb.net',
-    port: '',
+    port: 27017,
     user: 'superuser',
     password: 'superpass',
     database: 'khata'
   },
   sockets: {
-    onlyAllowOrigins: ['https://skillshape.netlify.com', 'http://localhost:8000','https://app-skillshape.herokuapp.com','https://app.skillshape.com']
+    onlyAllowOrigins: ['http://localhost:3000','https://khata-ryaz.herokuapp.com']
   },
   session:{
     cookie:{
