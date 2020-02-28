daec5d6bdffd500df69b14ee5b2b7d21cc4c472c
Satnam Singh <satnam9781@gmail.com>
2020-02-28 23:51:41 +0530

    Update code for saving invoice

--- api/services/invoice.js
+++ api/services/invoice.js
@@ -5,10 +5,34 @@ const { ACTION_FAILED, DATA_MISSING } = ERROR_TYPES;
 
 const create = async (req, res) => {
   try {
-    const { body: data } = req;
-    if(!data.invoiceNumber) {
+    const { invoiceNumber, project } = req.body;
+
+    if(!invoiceNumber || !project) {
       return sendBadRequest(res, DATA_MISSING);
     }
+    const { name, client, role, assignments, description } = project;
+    const total = assignments.reduce((a, b) => Number(a.price) + Number(b.price));
+    const items = [];
+    assignments.map((a) => {
+      items.push({
+        name: `${a.role} (${a.name})`,
+        hours: a.hours || '--',
+        unitPrice: a.price,
+        cost: a.price
+      });
+    });
+    const data = {
+      invoiceNumber,
+      project: {
+        name,
+        client,
+        role,
+      },
+      date: new Date().toDateString(),
+      description,
+      items,
+      total
+    };
 
     const invoice = await Invoice.create(data).fetch();
     if(!invoice) {
