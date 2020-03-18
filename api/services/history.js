e31f2c4f64daf66a416a7b35ec0ac915fe233f96
Satnam Singh <satnam9781@gmail.com>
2020-03-18 16:21:11 +0530

    Simplify history code

--- api/services/history.js
+++ api/services/history.js
@@ -4,52 +4,24 @@ const getHistory = async (req, res) => {
     const projects = await Project.find({});
     const history = [];
     members.forEach((m) => {
-      history.push({
-        name: m.name,
-        date: m.createdAt,
-        message: 'MEMBER_CREATED',
-      });
+      history.push({ name: m.name, date: m.createdAt, message: 'MEMBER_CREATED' });
       if(!m.active) {
-        history.push({
-          name: m.name,
-          date: m.updatedAt,
-          message: 'MEMBER_DELETED',
-        });
+        history.push({ name: m.name, date: m.updatedAt, message: 'MEMBER_DELETED' });
       }
     });
-
     projects.forEach((p) => {
-      history.push({
-        name: p.name,
-        date: p.createdAt,
-        message: 'PROJECT_CREATED',
-      });
+      history.push({ name: p.name, date: p.createdAt, message: 'PROJECT_CREATED' });
       if(!p.active) {
-        history.push({
-          name: p.name,
-          date: p.updatedAt,
-          message: 'PROJECT_DELETED',
-        })
+        history.push({ name: p.name, date: p.updatedAt, message: 'PROJECT_DELETED' });
       }
-
       p.assignments.forEach((a) => {
-        history.push({
-          name: p.name,
-          date: a.createdAt,
-          member: members.find((m) => m.id === a.id).name,
-          message: 'MEMBER_ADDED',
-        });
+        const member = members.find((m) => m.id === a.id).name;
+        history.push({ name: p.name, date: a.createdAt, member, message: 'MEMBER_ADDED' });
         if(!a.active) {
-          history.push({
-            name: p.name,
-            date: a.updatedAt,
-            member: members.find((m) => m.id === a.id).name,
-            message: 'MEMBER_REMOVED',
-          });
+          history.push({ name: p.name, date: a.updatedAt, member, message: 'MEMBER_REMOVED' });
         }
       });
     });
-
     history.sort((a, b) => a.date - b.date);
     res.send({
       success: true,
